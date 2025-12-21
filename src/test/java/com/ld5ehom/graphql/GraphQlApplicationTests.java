475f9316ba52f47e3b8ce3029570ed8a5bf69e4e
Taewook Park <155722702+ld5ehom@users.noreply.github.com>
2025-12-20 20:05:48 -0800

    first commit

---
+++ src/test/java/com/ld5ehom/graphql/GraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ld5ehom.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
